e4a07d5bb1c20d868548112fbce3ff46db108d45
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-06-10 04:23:13 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
-# I call on the method, say_hello, and give it the string "Gabriela" 
+# I call on the method, say_hello, and give it the string "Gabriela"
 say_hello("Gabriela")
- 
+
 # The method prints this text to the screen:
 Hello Gabriela!
 
