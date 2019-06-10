a30019ae7e5772c3f2696d49454c46c994fecdc8
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-06-10 04:38:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,3 @@
-def say_hello(name = "Ruby Programmer", i_love_soomin)
+def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
-  puts i_love_soomin
 end
-
-say_hello("jungi", "hi soomin")
