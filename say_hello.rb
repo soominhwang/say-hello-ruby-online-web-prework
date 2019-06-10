05d2884d6e5a3b2d63811e089484c6e517c77635
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-06-10 04:23:08 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,18 @@
-# Build your say_hello method here
+# I call on the method, say_hello, and give it the string "Gabriela" 
+say_hello("Gabriela")
+ 
+# The method prints this text to the screen:
+Hello Gabriela!
+
+
+
+  it 'accepts an argument of a name and prints out Hello with that Name' do
+    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
+    say_hello("Kent Beck")
+  end
+
+  it 'defaults to Ruby Programmer when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+    say_hello()
+  end
+end
